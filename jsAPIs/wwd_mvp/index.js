7d735d403241d644a38cf25ed89018f204d199bb
Levi Donaldson <lcdbacanas@gmail.com>
2020-04-14 19:40:51 -0500

    Update index.js

--- jsAPIs/wwd_mvp/index.js
+++ jsAPIs/wwd_mvp/index.js
@@ -10,7 +10,7 @@ const getInfo = () => {
                     `<div style="display: flex; background-color: #ccc; margin: 1rem auto;">
                         <div style="flex: 1; border-radius: 50%; background-color: orange; margin: 0 auto;"></div>
                         <ul style="background-color: teal; flex: 3;margin: 0;" >
-                            <li style="list-item-style: none; font-size: 1.5em;"> ${firstName}, ${lastName}</li>
+                            <li style="list-item-style: none; font-size: 1.5em;"> `${firstName} ${lastName}`</li>
                         </ul>
                     </div>`;
                 document.getElementById('result').innerHTML = result;
